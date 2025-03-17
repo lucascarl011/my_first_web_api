878428cda82e8ecb4e1b0857df301d3f8225b8af
lucascarl011 <lucassscarlosss54@gmail.com>
2025-03-16 22:01:06 -0300

    Criação UsuarioController

---
+++ src/main/java/dio/my_first_web_api/controller/UsuarioController.java
@@ -0,0 +1,42 @@
+package dio.my_first_web_api.controller;
+
+
+import dio.my_first_web_api.model.Usuario;
+import dio.my_first_web_api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @PostMapping()
+    public void post(@RequestBody Usuario usuario){
+        repository.save(usuario);
+    }
+
+    @PutMapping()
+    public void put(@RequestBody Usuario usuario){
+        repository.update(usuario);
+    }
+
+    @GetMapping()
+    public List<Usuario> getAll(){
+        return repository.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public Usuario getOne(@PathVariable("id") Integer id){
+        return repository.finById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+        repository.remove(id);
+    }
+}
